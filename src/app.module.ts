4d5eb9bd3f18c399854c8911b7e80e4d5afa3f9c
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2026-09-22 13:39:37 +0200

    feat : add config dependancy and files with zod check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,42 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { z } from 'zod';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: z.object({
+        PORT: z.coerce.number().int().min(1).max(65535).default(3000),
+        DB_HOST: z.string().min(1),
+        DB_PORT: z.coerce.number().int().min(1).max(65535),
+        DB_USERNAME: z.string().min(1),
+        DB_PASSWORD: z.string().min(1),
+        DB_DATABASE: z.string().min(1),
+      }),
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.getOrThrow<string>('DB_HOST'),
+        port: config.getOrThrow<number>('DB_PORT'),
+        username: config.getOrThrow<string>('DB_USERNAME'),
+        password: config.getOrThrow<string>('DB_PASSWORD'),
+        database: config.getOrThrow<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: false,
+        applicationName: 'veille-techno-backend',
+        retryAttempts: 1,
+        connectTimeoutMS: 5000,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
